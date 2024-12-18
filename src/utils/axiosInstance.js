f7d1c0b3c7cdcc722dfee2ca51ba582b686776ec
arman8808 <armanal3066@gmail.com>
2024-12-18 15:46:59 +0530

    commit

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: `${process.env.REACT_APP_API_URL}`,
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const accessToken = Cookies.get('accessToken');
+    const accessToken = Cookies.get("accessToken");
     if (accessToken) {
-      config.headers['Authorization'] = `Bearer ${accessToken}`;
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
     }
     return config;
   },
@@ -23,11 +23,17 @@ axiosInstance.interceptors.response.use(
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       try {
-        const res = await axios.post('/users/refresh-token', {}, {
-          withCredentials: true
-        });
-        Cookies.set('accessToken', res.data.accessToken);
-        originalRequest.headers['Authorization'] = `Bearer ${res.data.accessToken}`;
+        const res = await axios.post(
+          "/users/refresh-token",
+          {},
+          {
+            withCredentials: true,
+          }
+        );
+        Cookies.set("accessToken", res.data.accessToken);
+        originalRequest.headers[
+          "Authorization"
+        ] = `Bearer ${res.data.accessToken}`;
         return axiosInstance(originalRequest);
       } catch (err) {
         return Promise.reject(err);
