a82d9c4c7f22b3aadee78e56a01692403725d601
arman8808 <armanal3066@gmail.com>
2025-02-22 15:46:53 +0530

    chnage password viewer

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -387,7 +387,7 @@ const AuthForm = ({ stages, formType, handleFormSubmit, role }) => {
             onClick={() => setShowPassword(!showPassword)}
             className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600 focus:outline-none"
           >
-            {showPassword ? <FaEyeSlash className="text-textGray"/> : <FaEye className="text-textGray"/>}
+            {showPassword ? <FaEye className="text-textGray"/>  :<FaEyeSlash className="text-textGray"/> }
           </button>
         )}
       </div>
