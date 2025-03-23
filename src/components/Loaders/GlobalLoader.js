d6b2009509e39149f53660bbfae64d08ef8504d7
arman8808 <armanal3066@gmail.com>
2025-03-23 22:04:39 +0530

    commit

--- src/components/Loaders/GlobalLoader.js
+++ src/components/Loaders/GlobalLoader.js
@@ -9,9 +9,22 @@ const GlobalLoader = () => {
   if (!isLoading) return null;
 
   return (
-    <div className="loader-container">
-    <div className="loader"></div>
-  </div>
+    <Box
+      sx={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "rgba(255, 255, 255, 0.7)",
+        zIndex: 9999,
+      }}
+    >
+      <CircularProgress />
+    </Box>
   );
 };
 
